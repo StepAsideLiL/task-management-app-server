2b48cf46070ed5a3031d66a609f1739a9b9b5952
Rifat Khan <rifat6633@gmail.com>
2023-08-02 15:13:19 +0600

    create intry file server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+const app = express();
+const port = process.env.PORT || 3000;
+const uri = process.env.DB_URI;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Task Management App is running.");
+});
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Task Management App is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
